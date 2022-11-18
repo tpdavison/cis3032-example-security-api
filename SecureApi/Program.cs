f907edfff0f50578b1bf596cacf4d37d4d94c9c4
Tyrone Davison <t.p.davison@tees.ac.uk>
2022-11-18 22:53:11 +0000

    Add initial files

---
+++ SecureApi/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/api/values", () =>
+{
+    var rand = new Random().Next();
+    var value = new GetValuesDto(
+        Number: rand,
+        Message: $"Hello, your result is {rand}."
+    );
+    return value;
+});
+
+app.Run();
+
+record GetValuesDto(int Number, string Message);
