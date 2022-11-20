71c28094451fcd9cde5c3882f9861fc16c74af5b
Tyrone Davison <t.p.davison@tees.ac.uk>
2022-11-20 21:50:31 +0000

    Add auth checks

--- SecureApi/Program.cs
+++ SecureApi/Program.cs
@@ -1,7 +1,20 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["Auth:Authority"];
+        options.Audience = builder.Configuration["Auth:Audience"];
+    });
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -13,7 +26,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/values", () =>
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/api/values", [Authorize] () =>
 {
     var rand = new Random().Next();
     var value = new GetValuesDto(
